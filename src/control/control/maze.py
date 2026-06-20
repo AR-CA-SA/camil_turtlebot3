1e668f69505ebce64e53be34dc4180cd8441b5fd
PRG Wallace <prg@wallace.com>
2026-06-19 18:16:38 -0700

    that's it for today

--- src/control/control/maze.py
+++ src/control/control/maze.py
@@ -1,17 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import rclpy
 from rclpy.node import Node
-
-
+from matplotlib.animation import FuncAnimation
 from sensor_msgs.msg import LaserScan
 
 
 class MazeNode(Node):
     def __init__(self):
         super().__init__("maze_node")
-        self.lidar_data = self.create_subscription(LaserScan, '/scan', self.callback, 
+        self.subscription = self.create_subscription(LaserScan, '/scan', self.callback, 
          10)
     def callback(self, msg):
-        self.get_logger().info(f'Got {msg.ranges}')
+        self.lidar_data = np.array(msg.ranges)
+        self.lidar_data[np.isfinite(self.lidar_data)]  
+        self.get_logger().info(f'Got {self.lidar_data}')
+    def paser_lidar_data_2d(self):
+        degree = np.arange(0, 2*np.pi, 360)
+        x = self.lidar_data * np.cos(degree)
+        y = self.lidar_data * np.sin(degree)
+
+
+
+
 
 def main(args=None):
     rclpy.init(args=args)
